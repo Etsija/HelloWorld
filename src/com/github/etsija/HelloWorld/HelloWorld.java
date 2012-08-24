18abe162cdcd8e5d31d48f11f4886ba99b6afba7
Jyrki Keisala <jyrki.keisala@gmail.com>
2012-08-24 09:16:43 +0300

    First (empty) template.

---
+++ src/com/github/etsija/HelloWorld/HelloWorld.java
@@ -0,0 +1,16 @@
+package com.github.etsija.HelloWorld;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+// This plugin works as a personal playground and template for new plugins
+public class HelloWorld extends JavaPlugin {
+
+	public void OnEnable() {
+		
+	}
+	
+	public void OnDisable() {
+		
+	}
+	
+}
